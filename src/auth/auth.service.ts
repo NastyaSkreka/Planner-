2c001efa94fa3d8dc3234ab73c1160a3a32e382f
anastasiiaskreka <anastasiia.skr@cgsteam.io>
2024-10-07 15:09:27 +0300

    feat: add refresh token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,9 +8,13 @@ import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import { AuthDto } from './dto/auth.dto';
 import { verify } from 'argon2';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
+  EXPIRE_DAY_REFRESH_TOKEN = 1;
+  REFRESH_TOKEN_NAME = 'refreshToken';
+
   constructor(
     private jwt: JwtService,
     private userService: UserService,
@@ -34,6 +38,18 @@ export class AuthService {
     const { password, ...user } = await this.userService.create(dto);
     const tokens = this.issueTokens(user.id);
 
+    return {
+      user,
+      ...tokens,
+    };
+  }
+
+  async getNewTokens(refreshToken: string) {
+    const result = await this.jwt.verifyAsync(refreshToken);
+    if (!result) throw new UnauthorizedException('Invalid refresh token');
+
+    const { password, ...user } = await this.userService.getById(result.id);
+    const tokens = this.issueTokens(user.id);
     return {
       user,
       ...tokens,
@@ -67,6 +83,25 @@ export class AuthService {
   }
 
   addRefreshTokenToResponse(res: Response, refreshToken: string) {
-    
+    const expiresIn = new Date();
+    expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN);
+
+    res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+      httpOnly: true,
+      domain: 'localhost',
+      expires: expiresIn,
+      secure: true,
+      sameSite: 'none',
+    });
+  }
+
+  removeRefreshTokenFromResponse(res: Response) {
+    res.cookie(this.REFRESH_TOKEN_NAME, '', {
+      httpOnly: true,
+      domain: 'localhost',
+      expires: new Date(0),
+      secure: true,
+      sameSite: 'none',
+    });
   }
 }
