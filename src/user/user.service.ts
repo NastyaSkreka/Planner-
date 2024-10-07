3e39b4c2ed2f935d184b04ad814950592651c8aa
anastasiiaskreka <anastasiia.skr@cgsteam.io>
2024-10-07 11:40:14 +0300

    feat: init planner backend

---
+++ src/user/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  getById(id: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        tasks: true,
+      },
+    });
+  }
+}
